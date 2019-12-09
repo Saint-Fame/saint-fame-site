ce8481a492b538e9a0a939ccb56824300db0fc55
Dai Hovey <hoveydavid+daihovey+github@gmail.com>
2019-12-09 23:06:29 +1100

    Added ConnectWallet button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,14 @@ import { NextPage } from 'next'
 import Layout from '../components/Layout'
 import React from 'react'
 import Loader from '../components/Loader'
+import ConnectWalletButton from '../components/shop/ConnectWalletButton'
+import { Alert } from 'react-native'
 
 const Index: React.FunctionComponent = () => {
-
-return <Layout>Home</Layout>
+    return (
+        <Layout>
+            <ConnectWalletButton />
+        </Layout>
+    )
 }
 export default Index
