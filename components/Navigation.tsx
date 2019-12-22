8daea4410410b823dcdca7df9fadb9235bcb7a15
Tyson Battistella <tysonbattistella@gmail.com>
2019-12-22 12:22:21 -0800

    Enable Styled Components + Refactor Navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,50 +1,25 @@
+import React from 'react'
 import NavLink from './NavLink'
-import Link from 'next/link'
+import styled from 'styled-components'
+
+const NavigationContainer = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 100px;
+  flex-direction: column;
+  
+  width: 300px;
+  margin-right: 100px;
+`
 
 const Navigation = () => (
-    <div>
-        <span className="nav">
-            <NavLink href="/" children={<a>Home</a>} />
-            <NavLink href="/prophecy" children={<a>Prophecy</a>} />
-            <NavLink href="/voting" children={<a>Voting</a>} />
-            <NavLink href="/holders" children={<a>Tokens</a>} />
-            <NavLink href="/finances" children={<a>Finances</a>} />
-        </span>
-        <style jsx>{`
-            h1,
-            a {
-                font-family: 'Tenor Sans';
-                font-size: 25px;
-                line-height: 40px;
-                color: #fffafa;
-            }
-            a {
-                text-decoration: none;
-            }
-            a:hover {
-                opacity: 0.6;
-            }
-            .selected {
-                color: #ff00d6;
-            }
-            .selected::before {
-                background: #ff00d6;
-                transform: rotate(-45deg);
-                width: 14px;
-                height: 14px;
-                content: '';
-                position: absolute;
-                left: 79px;
-                margin-top: 13px;
-            }
-            .nav {
-                margin-left: 100px;
-                flex: 1;
-                display: flex;
-                flex-direction: column;
-            }
-        `}</style>
-    </div>
+    <NavigationContainer>
+      <NavLink href="/" children={'Home'} />
+      <NavLink href="/prophecy" children={'Prophecy'} />
+      <NavLink href="/voting" children={'Voting'} />
+      <NavLink href="/holders" children={'Tokens'} />
+      <NavLink href="/finances" children={'Finances'} />
+    </NavigationContainer>
 )
 
 export default Navigation
